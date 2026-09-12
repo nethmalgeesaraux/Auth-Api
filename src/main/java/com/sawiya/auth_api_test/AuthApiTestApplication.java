0202c2bb23fa82f8964fa880ab27ca45c6ef64a2
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-09-12 11:24:51 +0530

    initial commit

---
+++ src/main/java/com/sawiya/auth_api_test/AuthApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.sawiya.auth_api_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sawiya/auth_api_test/AuthApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sawiya.auth_api_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

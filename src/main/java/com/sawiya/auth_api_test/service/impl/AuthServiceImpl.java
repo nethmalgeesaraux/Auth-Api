3129ea73837f9c60fc2b5581f1cf0bf9a604775f
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-09-12 18:28:53 +0530

    Crete AuthServiceImpl

--- src/main/java/com/sawiya/auth_api_test/service/impl/AuthServiceImpl.java
+++ src/main/java/com/sawiya/auth_api_test/service/impl/AuthServiceImpl.java
@@ -2,15 +2,42 @@ package com.sawiya.auth_api_test.service.impl;
 
 import com.sawiya.auth_api_test.dto.AuthResponse;
 import com.sawiya.auth_api_test.dto.SignInRequest;
+import com.sawiya.auth_api_test.entity.User;
+import com.sawiya.auth_api_test.repository.UserRepository;
 import com.sawiya.auth_api_test.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
+    private final UserRepository userRepository;
+
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @Override
     public AuthResponse signIn(SignInRequest request) {
-        return null;
+
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() ->
+                        new RuntimeException("Invalid email or password"));
+
+        boolean passwordMatches = passwordEncoder.matches(
+                request.getPassword(),
+                user.getPassword()
+        );
+
+        if (!passwordMatches) {
+            throw new RuntimeException("Invalid email or password");
+        }
+
+        return new AuthResponse("Login successful",
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getCreatedAt()
+        );
     }
 }
